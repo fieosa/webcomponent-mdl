ee5c6279153d26bb20f08ec46de91f0fec4eb062
gk <gk@gks-MacBook-Pro.local>
2016-10-21 15:08:24 -0700

    Add build scripts and templates for docs.

---
+++ docs/_pages/started.md
@@ -0,0 +1,9 @@
+---
+layout: started
+title: Getting started
+include_prefix: ../
+---
+
+<section style="text-align:center;max-width: 1000px;margin:100px auto;">
+  <h3><b>Getting Started<b></h3>
+</section>
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,55 @@
 const webpack = require('webpack');
 
-const DEBUG = process.env.NODE_ENV !== 'production';
-
-module.exports = {
-  target: "web",
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: DEBUG ? 'test/' : 'dist/',
-    filename: DEBUG ? 'webcomponent-mdl.js' : 'webcomponent-mdl.min.js',
-  },
-  devServer:{
-    contentBase: 'docs/',
-    devtool: 'eval',
-    port: 5000,
-  },
-  devtool: DEBUG ? 'source-map' : false,
-  module: {
-    loaders: [
-      {
-        test: /\.(jsx|js)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules)/,
-      },
+module.exports = function (DEBUG) {
+  return {
+    target: "web",
+    entry: [
+      './src/index.js'
     ],
-  },
-  plugins: [
-
-    new webpack.optimize.OccurrenceOrderPlugin(true),
-
-    ...DEBUG ? [] : [
-
-      new webpack.optimize.DedupePlugin(),
-
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
+    output: {
+      // path: DEBUG ? 'test/' : 'dist/',
+      filename: DEBUG ? 'webcomponent-mdl.js' : 'webcomponent-mdl.min.js',
+    },
+    devServer:{
+      contentBase: 'docs/',
+      devtool: 'eval',
+      port: 5000,
+    },
+    devtool: DEBUG ? 'source-map' : false,
+    module: {
+      loaders: [
+        {
+          test: /\.(jsx|js)$/,
+          loader: 'babel-loader',
+          exclude: /(node_modules)/,
+          query: {
+            presets: ['es2015'],
+            plugins: [
+              ['transform-react-jsx', {
+                pragma: 'jsxdom'
+              }]
+            ]
+          }
         },
-        comment: {
-          comments: false,
-        }
-      }),
+      ],
+    },
+    plugins: [
+
+      new webpack.optimize.OccurrenceOrderPlugin(true),
+
+      ...DEBUG ? [] : [
+
+        new webpack.optimize.DedupePlugin(),
+
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            warnings: false,
+          },
+          comment: {
+            comments: false,
+          }
+        }),
+      ],
     ],
-  ],
-};
+  };
+}
