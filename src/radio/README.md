204717cc8855ccbafdd87b7be7d71644c540b09d
teaegg <teaegg.love@gmail.com>
2016-10-23 19:39:29 -0700

    Finish radio (#12)

---
+++ src/radio/README.md
@@ -0,0 +1,16 @@
+## Demo
+
+```html_demo
+<mdl-radio name="options" value="1" ripple>First</mdl-radio>
+<mdl-radio name="options" value="2" ripple checked>Second</mdl-radio>
+<mdl-radio name="options" value="3" ripple disabled>Second</mdl-radio>
+```
+
+## Configuration options
+
+| Element | Attribute | Type | Effect | Remarks |
+|---------|-----------|------|--------|---------|
+| mdl-Radio | `name` | String | Defines the name of the radio group | Required |
+| mdl-Radio | `disabled` | Boolean | Set as disabled | Optional |
+| mdl-Radio | `ripple` | Boolean | Applies the ripple effect | Optional |
+| mdl-Radio | `value` | String or Number | Defines the selected value | Required |
---
+++ src/radio/index.js
@@ -0,0 +1,52 @@
+import {
+  BaseCustomElement,
+  reflectPropertiesToAttributes,
+  mdlUpgrade,
+} from '../utils';
+
+class MaterialRadio extends BaseCustomElement {
+
+  createdCallback(children) {
+    this.classList.add('mdl-radio', 'mdl-js-radio');
+    this._input = <input type="radio" class="mdl-radio__button"/>;
+    this._onclick = this._onclick.bind(this);
+    <this>
+      {this._input}
+      <span class="mdl-radio__label">{children}</span>
+    </this>
+  }
+
+  connectedCallback() {
+    this.addEventListener('click', this._onclick);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('click', this._onclick);
+  }
+
+  _onclick() {
+    if (this.disabled) return;
+    this._input.checked = true;
+    this._input.dispatchEvent(new Event('change',{ bubbles: true }));
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    this.classList.toggle('mdl-js-ripple-effect', this.ripple);
+    this._input.checked = this.checked;
+    this._input.disabled = this.disabled;
+    this._input.value = this.value;
+    this._input.name = this.name;
+  }
+
+}
+
+export default reflectPropertiesToAttributes(
+  mdlUpgrade(MaterialRadio),
+  [
+    { propName: 'ripple', propType: Boolean },
+    { propName: 'disabled', propType: Boolean },
+    { propName: 'checked', propType: Boolean },
+    { propName: 'name', propType: String },
+    { propName: 'value', propType: String },
+  ]
+);
