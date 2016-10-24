04ccf8b8268f74f4ca4fdbcbffac9826e9186ceb
teaegg <teaegg.love@gmail.com>
2016-10-23 21:56:51 -0700

    Add footer

---
+++ src/footer/index.js
@@ -0,0 +1,4 @@
+export { default as MaterialFooter } from './footer';
+export { default as MaterialFooterSection } from './footer-section';
+export { default as MaterialFooterDropdownSection } from './footer-drop-down';
+export { default as MaterialFooterLinkList } from './footer-link-list';
